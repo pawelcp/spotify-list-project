01f964e6195d0d670192e0286bb0a0b56533c51d
pawelcp <94795184+pawelcp@users.noreply.github.com>
2022-12-28 00:01:56 +0100

    endless loooooooopppp

--- src/components/rankingBox.tsx
+++ src/components/rankingBox.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Flex, Spacer } from '@chakra-ui/react'
-import { Grid, GridItem, Text } from '@chakra-ui/react'
-import { Center } from '@chakra-ui/react'
+import { Grid, GridItem, Button, Center, Input, Flex, Box, Image, Spacer , Text, Link } from '@chakra-ui/react'
 import { IndexProps, RankingType } from '../models/RankingProps'
-import { SP } from 'next/dist/shared/lib/utils'
 import { getTop30 } from '../../lib/getTop30'
+import { SearchResponse } from '../models/searchTypes'
+import { getArtistSearch } from "../../lib/getArtistSearch";
+import {BsSpotify} from 'react-icons/bs'
 
 const RankingBox = () => {
 
   const [top30, setTop30] = useState<IndexProps>()
+  const [artistData, setArtistData] = useState<SearchResponse>()
   useEffect(()=>{
     try{
       const getTop30Handler = async() => { 
@@ -18,25 +19,64 @@ const RankingBox = () => {
     getTop30Handler()
     }catch(error:any){return error.message} 
   },[])
+  useEffect(() => {
+         const getSearchData = async () => {
+          if(top30?.data)
+            try{ 
+            const limit = '1'
+            for(const artistLoop of top30.data){
+            const data = await getArtistSearch(artistLoop.artist, limit)
+            setArtistData(data)}
+
+            }catch(error:any){error.message}
+        };getSearchData();
+},[])
+console.log(artistData);
+
+
+
+
   
       return (
         <Center>
-        <Grid templateColumns={{md:'repeat(3, 1fr)', sm:'repeat(1, 1fr)'}} marginTop='2vh'  w='80%' gap={50}>
-        {top30!==undefined&& top30.data.map((artist:RankingType) =>
-        {return( 
-        <GridItem w='100%'> 
-           <Box  boxShadow='2xl'   w='100%' borderRadius='lg' bg='blackAlpha.800' h='12vh' overflow='hidden'>  
-               <Flex>
-                 <Center>
-                 <Text fontSize='3xl' ml='4' fontWeight='medium' p='2' color='white'>{artist.artist}</Text> 
-                 </Center>
-                 <Spacer ></Spacer>
-                 <Flex flexDirection='column' mr='3' mt='2'>
-                 <Text fontSize='md' textAlign='center' color='white'>Rank</Text>
-                 <Text fontSize='4xl'textAlign='center' color='white' >{artist.rank}</Text>
-                 </Flex>
-               </Flex>
-           </Box>
+        <Grid m='auto' templateColumns={{md:'repeat(3, 1fr)', sm:'repeat(1, 1fr)'}} marginTop='2vh'  w='90%' gap='4'>
+        {top30!==undefined && top30.data.map((artist:RankingType) =>
+        {return(
+        <GridItem w='60%'>
+           {top30!==undefined && artistData?.artists.items.map((artistData) => {
+                return (
+                <Center>
+                    <Box rounded='lg' boxShadow='dark-lg' w={{xl:'30vw',md:'50vw', sm:'70vw'}} h='150' textColor='white' mt='4' bg='blackAlpha.800'>
+                        <Flex>
+                            <Center>
+                            <Box w='60' maxW='190px' >
+                                <Center>
+                                {artistData.data.profile.name.length < 17 && <Text fontSize='2xl' fontWeight='medium'>{artistData.data.profile.name}</Text>}
+                                {artistData.data.profile.name.length > 17 && <Text fontSize='xl' fontWeight='medium'>{artistData.data.profile.name}</Text>}
+                                </Center>
+                                <Center>
+                                <Link color='green.500' href={artistData.data.uri}>Listen on spotify
+                                    <Center>
+                                    <BsSpotify></BsSpotify>
+                                    </Center>
+                                </Link>
+                                </Center>
+                            </Box>
+                                <Flex flexDirection='column' mr='3' mt='2'>
+                                  <Text fontSize='md' textAlign='center' color='white'>Rank</Text>
+                                  <Text fontSize='4xl'textAlign='center' color='white' >{artist.rank}</Text>
+                                </Flex>
+                            </Center>
+                            <Spacer></Spacer>
+                            {artistData.data.visuals.avatarImage !== null && 
+                            <Image rounded='lg' h={{md:'150',sm:'150'}} maxH='150' minW={{sm:'150', md:'auto'}} src={artistData.data.visuals.avatarImage.sources[0].url}></Image>}
+                            {artistData.data.visuals.avatarImage === null && 
+                            <Image rounded='lg' h={{md:'150',sm:'150'}} maxH='150' minW={{sm:'150', md:'auto'}} src='https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png'></Image>}
+                            
+                        </Flex>
+                    </Box>
+                </Center>)}
+            )}
         </GridItem>)} )}
         </Grid>
         </Center>
